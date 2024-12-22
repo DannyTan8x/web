44b27ed33ee214ef56673aa50f8f5cb27636537c
Danny Tan <129268549+DannyTan8x@users.noreply.github.com>
2024-12-22 18:45:55 +0800

    add Global Styles components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 
+import GlobalStyles from './components/GlobalStyles';
+
 const App = () => {
   return (
     <div>
+      <GlobalStyles />
       <Pages />
     </div>
   );
