9f2e107a463cd8a5bd255f21a3dc2abef830efac
Danny Tan <129268549+DannyTan8x@users.noreply.github.com>
2025-01-10 23:10:55 +0800

    connect localhost API
    -add component : Note; NoteFeed to render data from API

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@
 // This is the main entry point of our application
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pages from './pages';
 
+//匯入 Apollo 函式庫
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+//匯入路徑
+import Pages from './pages';
+//全域樣式
 import GlobalStyles from './components/GlobalStyles';
 
-const App = () => {
-  return (
-    <div>
-      <GlobalStyles />
-      <Pages />
-    </div>
-  );
-};
+//配置API URI 和快取
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+//配置 Apollo Client
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
+const App = () => (
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
